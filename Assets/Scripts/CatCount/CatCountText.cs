b788c596ba570967ed948308543dc97a5639c588
MrInformatic <maxundmoritz99199@gmail.com>
2019-12-07 18:01:51 +0100

    Added UI

---
+++ Assets/Scripts/CatCount/CatCountText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class CatCountText : MonoBehaviour
+{
+    public Area area;
+    private Text text;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+        area.onCatCountChangeEvent.AddListener(OnCatCountChange);
+    }
+
+    void OnCatCountChange() 
+    {
+        text.text = "Cat Count: " + area.CatCount;
+    }
+}
---
+++ Assets/Scripts/UI/ChangeSceneOnClick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Button))]
+public class ChangeSceneOnClick : MonoBehaviour
+{
+    public string sceneName;
+
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(OnClickButton);
+    }
+
+    void OnClickButton()
+    {
+        SceneManager.LoadScene(sceneName);        
+    }
+}
