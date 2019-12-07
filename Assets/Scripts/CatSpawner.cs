5a64c45e34648c2ae499a46b9dfe9aa2b79d9189
MrInformatic <maxundmoritz99199@gmail.com>
2019-12-07 21:34:52 +0100

    Super

---
+++ Assets/Scripts/CatSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatSpawner : MonoBehaviour
+{
+    public GameObject cat;
+    public int count;
+
+    void Start() 
+    {
+        for(int i = 0; i < count; i++) 
+        {
+            Instantiate(cat);
+        }
+    }
+}
